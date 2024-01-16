d3f26a3440d54ce0e9d7be8b8d496c0d5727b9ee
Shivam <shivamrai6836@gmail.com>
2024-01-16 22:09:47 +0530

    storing player volume in local storage

--- hooks/useVolume.js
+++ hooks/useVolume.js
@@ -1,6 +1,9 @@
 import { create } from "zustand";
 
 export const useVolume = create((set)=>({
-    volume : 1,
-    setVolume : (volume)=>set({volume})
+    volume : Number(localStorage.getItem("spotify-player-volume")) || 1,
+    setVolume : (volume)=>{
+        set({volume});
+        localStorage.setItem("spotify-player-volume", `${volume}`);
+    }
 }));
